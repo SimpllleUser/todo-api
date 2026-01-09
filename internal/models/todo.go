1a4822b9824817440f8cdbd793444bf4d4802107
Vitalii <v.pecheritsya@upstars.com>
2026-01-09 23:09:03 +0200

    refactor: destructurization of todo fields

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -1,17 +1,18 @@
 package model
 
-type TodoModel struct {
+type TodoBaseFields struct {
 	ID          uint   `gorm:"primaryKey" json:"id" example:"1"`
 	Title       string `gorm:"not null" json:"title" validate:"required|minLen:3" filter:"trim" message:"required:{field} is required" label:"Title" example:"Buy groceries"`
 	Description string `gorm:"null" json:"description" example:"Milk, eggs, bread"`
 	Completed   bool   `gorm:"default:false" json:"completed" example:"false"`
-	UserId      uint   `gorm:"not null" json:"user_id" example:"1"`
 }
 
+type TodoModel struct {
+	TodoBaseFields
+	UserId uint `gorm:"not null" json:"user_id" example:"1"`
+}
 type TodoCreateRequest struct {
-	Title       string `gorm:"not null" json:"title" validate:"required|minLen:3" filter:"trim" message:"required:{field} is required" label:"Title" example:"Buy groceries"`
-	Description string `gorm:"null" json:"description" example:"Milk, eggs, bread"`
-	Completed   bool   `gorm:"default:false" json:"completed" example:"false"`
+	TodoBaseFields
 }
 
 func (TodoModel) TableName() string {
