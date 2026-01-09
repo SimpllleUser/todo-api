2a8945e102f11b1a155fde2a09c2ac0bbbf3ac8d
Vitalii <v.pecheritsya@upstars.com>
2026-01-09 23:17:53 +0200

    feat: add projectId field for todo

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -5,6 +5,11 @@ type TodoBaseFields struct {
 	Title       string `gorm:"not null" json:"title" validate:"required|minLen:3" filter:"trim" message:"required:{field} is required" label:"Title" example:"Buy groceries"`
 	Description string `gorm:"null" json:"description" example:"Milk, eggs, bread"`
 	Completed   bool   `gorm:"default:false" json:"completed" example:"false"`
+	TodoProjectId
+}
+
+type TodoProjectId struct {
+	ProjectId uint `gorm:"not null" json:"project_id" example:"1"`
 }
 
 type TodoModel struct {
